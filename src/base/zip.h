2f8b0afd1cd0ff6c03e7193c4da437215a291abd
Xu Shaohua <shaohua@biofan.org>
2021-05-18 15:23:52 +0800

    base: Add zip lib

---
+++ src/base/zip.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+#ifndef RUSTY_SRC_BASE_ZIP_H_
+#define RUSTY_SRC_BASE_ZIP_H_
+
+#include <QtGui/private/qzipreader_p.h>
+#include <QtGui/private/qzipwriter_p.h>
+
+#endif  // RUSTY_SRC_BASE_ZIP_H_
