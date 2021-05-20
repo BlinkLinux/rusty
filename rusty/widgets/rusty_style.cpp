9e5cb1368e642961201ae9b1d1260309275ed999
Xu Shaohua <shaohua@biofan.org>
2021-05-20 16:58:05 +0800

    widgets: Add rusty style

---
+++ rusty/widgets/rusty_style.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+#include "rusty/widgets/rusty_style.h"
+
+namespace rusty {
+
+RustyStyle::RustyStyle(QObject* parent) : QProxyStyle(parent) {
+
+}
+
+}  // namespace rusty
---
+++ rusty/widgets/rusty_style.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+#ifndef RUSTY_RUSTY_WIDGETS_RUSTY_STYLE_H_
+#define RUSTY_RUSTY_WIDGETS_RUSTY_STYLE_H_
+
+#include <QProxyStyle>
+
+namespace rusty {
+
+class RustyStyle : public QProxyStyle {
+  Q_OBJECT
+ public:
+  explicit RustyStyle(QObject* parent = nullptr);
+  ~RustyStyle() override = default;
+};
+
+}  // namespace rusty
+
+#endif  // RUSTY_RUSTY_WIDGETS_RUSTY_STYLE_H_
