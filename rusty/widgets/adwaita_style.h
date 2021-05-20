710105751434bf9bec0f879cfcc11bca8fb84181
Xu Shaohua <shaohua@biofan.org>
2021-05-20 18:02:55 +0800

    widgets: Add AdwaitaStyle

---
+++ rusty/widgets/adwaita_style.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#ifndef RUSTY_RUSTY_WIDGETS_ADWAITA_STYLE_H_
+#define RUSTY_RUSTY_WIDGETS_ADWAITA_STYLE_H_
+
+#include "rusty/third_party/adwaita-qt/src/style/adwaitastyle.h"
+
+namespace rusty {
+
+// Re-export adwaita style class.
+using AdwaitaStyle = Adwaita::Style;
+
+}  // namespace rusty
+
+#endif  // RUSTY_RUSTY_WIDGETS_ADWAITA_STYLE_H_
