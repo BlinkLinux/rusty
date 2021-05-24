3f5ea93f0a846bc3857129c4c8456509c81513c8
Xu Shaohua <shaohua@biofan.org>
2021-05-25 07:50:33 +0800

    widgets: Add left panel buttons

---
+++ rusty/widgets/nav/left_panel_buttons.cpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU Lesser General Public License
+// that can be found in the LICENSE file.
+
+#include "rusty/widgets/nav/left_panel_buttons.h"
+
+#include "rusty/widgets/nav/tab_button.h"
+
+namespace rusty {
+
+LeftPanelButtons::LeftPanelButtons(QWidget* parent) : QFrame(parent) {
+  this->initUi();
+  this->initSignals();
+}
+
+void LeftPanelButtons::initUi() {
+  this->button_group_ = new QButtonGroup(this);
+  this->button_layout_ = new QVBoxLayout();
+  this->button_layout_->setSpacing(0);
+  this->button_layout_->setContentsMargins(0, 0, 0, 0);
+  this->setLayout(this->button_layout_);
+}
+
+void LeftPanelButtons::initSignals() {
+  connect(this->button_group_, &QButtonGroup::idClicked, [=](int button_id) {
+    emit this->activeChanged(button_id);
+  });
+}
+
+void LeftPanelButtons::addButton(int button_id, const QString& icon, const QString& text) {
+  auto* button = new TabButton(icon, text);
+  this->button_layout_->addWidget(button);
+  this->button_group_->addButton(button, button_id);
+}
+
+void LeftPanelButtons::addStretch() {
+  this->button_layout_->addStretch();
+}
+
+int LeftPanelButtons::activeButton() const {
+  return this->button_group_->checkedId();
+}
+
+void LeftPanelButtons::setActiveButton(int button_id) {
+  this->button_group_->button(button_id)->click();
+}
+
+}  // namespace rusty
---
+++ rusty/widgets/nav/left_panel_buttons.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU Lesser General Public License
+// that can be found in the LICENSE file.
+
+#ifndef RUSTY_RUSTY_WIDGETS_NAV_LEFT_PANEL_BUTTONS_FRAME_H_
+#define RUSTY_RUSTY_WIDGETS_NAV_LEFT_PANEL_BUTTONS_FRAME_H_
+
+#include <QButtonGroup>
+#include <QFrame>
+#include <QVBoxLayout>
+
+namespace rusty {
+
+class LeftPanelButtons : public QFrame {
+  Q_OBJECT
+ public:
+  explicit LeftPanelButtons(QWidget* parent = nullptr);
+  ~LeftPanelButtons() override = default;
+
+  void addButton(int button_id, const QString& icon, const QString& text);
+
+  void addStretch();
+
+  [[nodiscard]] int activeButton() const;
+
+ public slots:
+  void setActiveButton(int button_id);
+
+ signals:
+  void activeChanged(int button_id);
+
+ private:
+  void initUi();
+  void initSignals();
+
+  QButtonGroup* button_group_{nullptr};
+  QVBoxLayout* button_layout_{nullptr};
+};
+
+}  // namespace rusty
+
+#endif  // RUSTY_RUSTY_WIDGETS_NAV_LEFT_PANEL_BUTTONS_FRAME_H_
