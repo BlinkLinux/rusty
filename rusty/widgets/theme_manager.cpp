51ee215eece5530c70192c9bf1f695354a930d90
Xu Shaohua <shaohua@biofan.org>
2021-05-24 11:17:03 +0800

    widgets: Add themeManager

---
+++ rusty/widgets/theme_manager.cpp
@@ -0,0 +1,94 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU Lesser General Public License
+// that can be found in the LICENSE file.
+
+#include "rusty/widgets/theme_manager.h"
+
+#include <QSharedPointer>
+
+namespace rusty {
+namespace {
+
+constexpr const char* kThemeDark = "dark";
+constexpr const char* kThemeLight = "light";
+constexpr const char* kThemeManagerIcon = "_r_theme_manager_icon_";
+
+}  // namespace
+
+ThemeManager::ThemeManager(QObject* parent) : QObject(parent) {
+
+}
+
+// static
+ThemeManager& ThemeManager::instance() {
+  static QSharedPointer<ThemeManager> instance{new ThemeManager()};
+  static ThemeManager& instance_ref{*instance};
+  return instance_ref;
+}
+
+bool ThemeManager::updateIconPath(QString& icon_path, ThemeManager::ThemeType theme_type) {
+  if (theme_type == ThemeType::kDark) {
+    if (icon_path.contains(kThemeLight)) {
+      icon_path.replace(kThemeLight, kThemeDark);
+      return true;
+    }
+  } else {
+    if (icon_path.contains(kThemeDark)) {
+      icon_path.replace(kThemeDark, kThemeLight);
+      return true;
+    }
+  }
+  return false;
+}
+
+ThemeManager::ThemeType ThemeManager::themeType() const {
+  return this->theme_type_;
+}
+
+void ThemeManager::setThemeType(ThemeManager::ThemeType theme_type) {
+  if (this->theme_type_ != theme_type) {
+    this->theme_type_ = theme_type;
+    emit this->themeTypeChanged(theme_type);
+  }
+}
+
+void ThemeManager::setThemeType(ThemeManager::ThemeType theme_type, bool force_update) {
+  if (force_update || this->theme_type_ != theme_type) {
+    this->theme_type_ = theme_type;
+    emit this->themeTypeChanged(theme_type);
+  }
+}
+
+void ThemeManager::registerAction(QAction* action, const QString& icon_name) {
+  Q_ASSERT(action != nullptr);
+  action->setProperty(kThemeManagerIcon, icon_name);
+  auto conn = connect(this, &ThemeManager::themeTypeChanged, [=](ThemeManager::ThemeType theme_type) {
+    QString last_icon_name = action->property(kThemeManagerIcon).toString();
+    const bool changed = ThemeManager::updateIconPath(last_icon_name, theme_type);
+    if (changed) {
+      action->setProperty(kThemeManagerIcon, last_icon_name);
+      action->setIcon(QIcon(last_icon_name));
+    }
+  });
+  connect(action, &QAction::destroyed, [conn]() {
+    disconnect(conn);
+  });
+}
+
+void ThemeManager::registerWidget(QObject* object, const QString& icon_name, ThemeManager::Callback callback) {
+  Q_ASSERT(object != nullptr);
+  object->setProperty(kThemeManagerIcon, icon_name);
+  auto conn = connect(this, &ThemeManager::themeTypeChanged, [=](ThemeManager::ThemeType theme_type) {
+    QString last_icon_name = object->property(kThemeManagerIcon).toString();
+    const bool changed = ThemeManager::updateIconPath(last_icon_name, theme_type);
+    if (changed) {
+      object->setProperty(kThemeManagerIcon, last_icon_name);
+    }
+    callback(changed, last_icon_name);
+  });
+  connect(object, &QAction::destroyed, [conn]() {
+    disconnect(conn);
+  });
+}
+
+}  // namespace rusty
---
+++ rusty/widgets/theme_manager.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU Lesser General Public License
+// that can be found in the LICENSE file.
+
+#ifndef RUSTY_RUSTY_WIDGETS_THEME_MANAGER_H_
+#define RUSTY_RUSTY_WIDGETS_THEME_MANAGER_H_
+
+#include <functional>
+#include <QAction>
+#include <QObject>
+
+namespace rusty {
+
+class ThemeManager : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(ThemeType themeType READ themeType WRITE setThemeType NOTIFY themeTypeChanged);
+ public:
+  enum ThemeType {
+    kLight,
+    kDark,
+  };
+  Q_ENUM(ThemeType);
+
+  ~ThemeManager() override = default;
+
+  static ThemeManager& instance();
+
+  [[nodiscard]] ThemeType themeType() const;
+
+  void setThemeType(ThemeType theme_type);
+  void setThemeType(ThemeType theme_type, bool force_update);
+
+  static bool updateIconPath(QString& icon_path, ThemeType theme_type);
+
+  void registerAction(QAction* action, const QString& icon_name);
+
+  using Callback = std::function<void(bool changed, const QString& icon_name)>;
+  void registerWidget(QObject* object, const QString& icon_name, Callback callback);
+
+ signals:
+  void themeTypeChanged(ThemeType theme_type);
+
+ private:
+  explicit ThemeManager(QObject* parent = nullptr);
+  ThemeType theme_type_{};
+};
+
+}  // namespace rusty
+
+#endif  // RUSTY_RUSTY_WIDGETS_THEME_MANAGER_H_
