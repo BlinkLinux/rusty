6bb6952e3c73850d5e9261d93a42f0b9252f091e
Xu Shaohua <shaohua@biofan.org>
2021-06-25 09:31:54 +0800

    widgets: Add form section

---
+++ rusty/widgets/form_section.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#ifndef RUSTY_RUSTY_WIDGETS_FORM_SECTION_H_
+#define RUSTY_RUSTY_WIDGETS_FORM_SECTION_H_
+
+#include <QFrame>
+
+namespace rusty {
+
+class FormSection : public QFrame {
+  Q_OBJECT
+ public:
+  explicit FormSection(QWidget* parent = nullptr);
+  ~FormSection() override = default;
+};
+
+}  // namespace rusty
+
+#endif  // RUSTY_RUSTY_WIDGETS_FORM_SECTION_H_
